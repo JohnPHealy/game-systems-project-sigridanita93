09a2dfcca2f0ae5209922db47bf5f91e555a5af4
sigridanita93 <79100365+sigridanita93@users.noreply.github.com>
2021-05-04 18:39:03 +0200

    New Scene
    Numbers riddle

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,44 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    private int num;
+
+    private int countGuess;
+
+    [SerializeField]
+    private InputField input;
+
+    [SerializeField]
+    private Text text;
+
+
+    void Awake () {
+        //   {input = GameObject.Find ("InputField").GetComponent<InputField>();
+        num = Random.Range(0,200);
+        text.text = "Guess A Number Between 0 and 200";
+         }
+
     public void GetInput(string guess)
     {
-        Debug.Log("You Entered" + guess);
+        CompareGuesses(int.Parse(guess));
+        input.text = "";
+    }
+
+    void CompareGuesses(int guess)
+    {
+        if(guess == num)
+        {
+            text.text = "You Guessed Correctly The Number Was" + guess;
+        } else if (guess < num) 
+        {
+            text.text = "Your Guess Number is Less Than The Number You Are Trying to Guess";
+        } else if (guess > num)
+        {
+            text.text = "Your Guess Number is Greater Than The Number You Are Trying to Guess";
+        }
+   
     }
 }
